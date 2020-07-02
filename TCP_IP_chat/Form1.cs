ec9efb5f45dacf3ffd863bbaf7e9d8b90d0e1934
Artur Kowalewski <art.kowalewski1@gmail.com>
2020-07-02 15:37:08 +0200

    Czat TCP/IP

---
+++ TCP_IP_chat/Form1.cs
@@ -0,0 +1,59 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_IP_chat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+        
+       private void Form1_Load(object sender, EventArgs e)
+       {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;//enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+
+       }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+              {
+                  txtStatus.Text += e.MessageString;
+                  e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+              });
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtStatus.Text += "Server starting...";
+            System.Net.IPAddress ip = new System.Net.IPAddress(long.Parse(txtHost.Text));
+            server.Start(ip, Convert.ToInt32(txtPort.Text));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+                server.Stop();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
